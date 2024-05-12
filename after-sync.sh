997454617a67d9790bbebdba4d63d58d29266f2d
xenxynon <flynryder427@gmail.com>
2024-05-12 12:31:10 +0530

    Update after-sync.sh

--- after-sync.sh
+++ after-sync.sh
@@ -11,4 +11,4 @@ git clone https://github.com/Los-Ext/android_frameworks_base -b kek frameworks/b
 git clone https://github.com/Los-Ext/android_vendor_lineage -b SDC vendor/lineage --depth 1
 git clone https://github.com/Los-Ext/android_frameworks_native -b lineage-21.0 frameworks/native --depth 1
 git clone https://bitbucket.org/xenxynon/vendor_nothing_spacewar_radio -b 2.5.5 vendor/nothing/Spacewar/radio --depth 1
-sed -i 's/PTC_ub1/LA.UM.9.14.r1-24200-LAHAINA.QSSI13.0/' kernel/nothing/sm7325/arch/arm64/configs/vendor/lahaina-qgki_defconfig &&
+sed -i 's/PTC_ub1/LA.UM.9.14.r1-24200-LAHAINA.QSSI13.0/g' kernel/nothing/sm7325/arch/arm64/configs/vendor/lahaina-qgki_defconfig &&
