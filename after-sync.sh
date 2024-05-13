87cc70a986bd574d22bd41b6face80ca4b8e159e
xenxynon <flynryder427@gmail.com>
2024-05-13 11:44:37 +0530

    Update after-sync.sh

--- after-sync.sh
+++ after-sync.sh
@@ -2,12 +2,12 @@ rm -rf vendor/gapps/* packages/apps/Settings vendor/lineage vendor/google/gms ve
 git clone https://github.com/xenxynon/device_nothing_Spacewar -b los device/nothing/Spacewar
 git clone https://gitlab.com/yaosp/vendor_google_gms -b fourteen vendor/google/gms
 git clone https://gitlab.com/xenxynon/yaosp -b fourteen vendor/google/pixel
-git clone https://github.com/PixysOS-Devices/kernel_nothing_sm7325 kernel/nothing/sm7325
+git clone https://github.com/PixysOS-Devices/kernel_nothing_sm7325 kernel/nothing/sm7325 --depth 1
 git clone https://gitlab.com/PixysOS/vendor_nothing_camera -b fourteen vendor/nothing/camera
-git clone https://github.com/PixysOS-Devices/vendor_nothing_Spacewar vendor/nothing/Spacewar
-git clone https://github.com/Los-Ext/android_packages_apps_Settings -b test packages/apps/Settings
+git clone https://github.com/PixysOS-Devices/vendor_nothing_Spacewar vendor/nothing/Spacewar --depth 1
+git clone https://github.com/Los-Ext/android_packages_apps_Settings -b test packages/apps/Settings --depth 1
 git clone https://github.com/PixysOS-Devices/packages_apps_PixysGlyph packages/apps/Glyph
-git clone https://github.com/Los-Ext/android_frameworks_base -b kek frameworks/base
+git clone https://github.com/Los-Ext/android_frameworks_base -b kek frameworks/base --depth 1
 git clone https://github.com/Los-Ext/android_vendor_lineage -b SDC vendor/lineage
 git clone https://github.com/Los-Ext/android_frameworks_native -b lineage-21.0 frameworks/native
 git clone https://bitbucket.org/xenxynon/vendor_nothing_spacewar_radio -b 2.5.5 vendor/nothing/Spacewar/radio
