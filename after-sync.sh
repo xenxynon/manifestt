4c7805f82fc46823c7d51aa6518b5f018cc66e25
xenxynon <flynryder427@gmail.com>
2024-05-13 16:42:08 +0530

    Update after-sync.sh

--- after-sync.sh
+++ after-sync.sh
@@ -1,4 +1,4 @@
-rm -rf packages/apps/Settings vendor/lineage vendor/google/gms vendor/google/pixel frameworks/base vendor/gapps packages/apps/Settings frameworks/base vendor/nothing* device/nothing* kernel/nothing* packages/apps/Glyph frameworks/native 
+rm -rf art packages/apps/Settings vendor/lineage vendor/google/gms vendor/google/pixel frameworks/base vendor/gapps packages/apps/Settings frameworks/base vendor/nothing* device/nothing* kernel/nothing* packages/apps/Glyph frameworks/native 
 git clone https://github.com/xenxynon/device_nothing_Spacewar -b los device/nothing/Spacewar
 #git clone https://gitlab.com/yaosp/vendor_google_gms -b fourteen vendor/google/gms
 #git clone https://gitlab.com/xenxynon/yaosp -b fourteen vendor/google/pixel
@@ -11,4 +11,5 @@ git clone https://github.com/Los-Ext/android_frameworks_base -b kek frameworks/b
 git clone https://github.com/Los-Ext/android_vendor_lineage -b SDC vendor/lineage
 git clone https://github.com/Los-Ext/android_frameworks_native -b lineage-21.0 frameworks/native --depth 1
 git clone https://bitbucket.org/xenxynon/vendor_nothing_spacewar_radio -b 2.5.5 vendor/nothing/Spacewar/radio --depth 1
+git clone https://github.com/xenxynon/manifestt -b lineage-19.1 art
 sed -i 's/PTC_ub1/LA.UM.9.14.r1-24200-LAHAINA.QSSI13.0/g' kernel/nothing/sm7325/arch/arm64/configs/vendor/lahaina-qgki_defconfig
