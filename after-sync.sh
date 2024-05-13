b5e056a89dd03aa70285c73b747c5e00328c09d9
xenxynon <flynryder427@gmail.com>
2024-05-13 14:35:03 +0530

    Update after-sync.sh

--- after-sync.sh
+++ after-sync.sh
@@ -3,12 +3,12 @@ git clone https://github.com/xenxynon/device_nothing_Spacewar -b los device/noth
 #git clone https://gitlab.com/yaosp/vendor_google_gms -b fourteen vendor/google/gms
 #git clone https://gitlab.com/xenxynon/yaosp -b fourteen vendor/google/pixel
 git clone https://github.com/PixysOS-Devices/kernel_nothing_sm7325 kernel/nothing/sm7325 --depth 1
-git clone https://gitlab.com/PixysOS/vendor_nothing_camera -b fourteen vendor/nothing/camera
+git clone https://gitlab.com/PixysOS/vendor_nothing_camera -b fourteen vendor/nothing/camera --depth 1
 git clone https://github.com/PixysOS-Devices/vendor_nothing_Spacewar vendor/nothing/Spacewar --depth 1
 git clone https://github.com/Los-Ext/android_packages_apps_Settings -b test packages/apps/Settings --depth 1
 git clone https://github.com/PixysOS-Devices/packages_apps_PixysGlyph packages/apps/Glyph
 git clone https://github.com/Los-Ext/android_frameworks_base -b kek frameworks/base --depth 1
 git clone https://github.com/Los-Ext/android_vendor_lineage -b SDC vendor/lineage
-git clone https://github.com/Los-Ext/android_frameworks_native -b lineage-21.0 frameworks/native
-git clone https://bitbucket.org/xenxynon/vendor_nothing_spacewar_radio -b 2.5.5 vendor/nothing/Spacewar/radio
+git clone https://github.com/Los-Ext/android_frameworks_native -b lineage-21.0 frameworks/native --depth 1
+git clone https://bitbucket.org/xenxynon/vendor_nothing_spacewar_radio -b 2.5.5 vendor/nothing/Spacewar/radio --depth 1
 sed -i 's/PTC_ub1/LA.UM.9.14.r1-24200-LAHAINA.QSSI13.0/g' kernel/nothing/sm7325/arch/arm64/configs/vendor/lahaina-qgki_defconfig
